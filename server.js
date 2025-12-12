35fb727473bea4723076665cf31bccea9d924692
Syed Azadar Hussain <syedazadarhussayn@gmail.com>
2025-12-12 23:39:49 +0530

    Fix: Add journey route and fix all API endpoints

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ const io = new Server(server, {
     },
     methods: ["GET", "POST"],
     credentials: false                // required when allowing all origins
-  }
+  },
 
   path: '/socket.io/',
   transports: ["websocket"],  
